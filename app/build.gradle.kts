efa0cfa9abc7ced53a0a1dc79392adf3aae029b5
sebas030903 <rodrigo.gutierrez.v@tecsup.edu.pe>
2025-05-05 10:55:47 -0500

    Añadimos Dependencias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,8 +40,11 @@ android {
     }
 }
 
+val room_version = "2.6.1"
 dependencies {
 
+    implementation("androidx.room:room-ktx:$room_version")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$room_version")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
